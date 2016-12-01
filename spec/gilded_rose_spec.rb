eb5d696055c869bec6414007db8adac13d3c20cb
Katy McCann <katy.alice.mccann@gmail.com>
2016-12-01 14:43:30 +0000

    Aged Brie, increases in quality by one as it approaces its sell by date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -11,22 +11,34 @@ describe GildedRose do
       expect(items[0].name).to eq "fixme"
     end
 
-    it "degrades in quality by one each day as it approaches its sell by date" do
-      items = [Item.new("+5 Dexterity Vest", 10, 20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq(19)
+    context '+5 Dexterity Vest' do
+      it "degrades in quality by one each day as it approaches its sell by date" do
+        items = [Item.new("+5 Dexterity Vest", 10, 20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(19)
+      end
+
+      it "degrades twice as fast after its sell by date" do
+        items = [Item.new("+5 Dexterity Vest", 10, 20)]
+        12.times {GildedRose.new(items).update_quality()}
+        expect(items[0].quality).to eq(6)
+      end
+
+      it "the quality is never negative" do
+        items = [Item.new("+5 Dexterity Vest", 10, 20)]
+        25.times {GildedRose.new(items).update_quality()}
+        expect(items[0].quality).to eq(0)
+      end
     end
 
-    it "degrades twice as fast after its sell by date" do
-      items = [Item.new("+5 Dexterity Vest", 10, 20)]
-      12.times {GildedRose.new(items).update_quality()}
-      expect(items[0].quality).to eq(6)
-    end
+      context 'Aged Brie' do
+        it "increases in quality by one, each day as, it approaches its sell by date" do
+          items = [Item.new("Aged Brie", 2, 0)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq(1)
+        end
 
-    it "the quality of +5 Dexterity Vest is never negative" do
-      items = [Item.new("+5 Dexterity Vest", 10, 20)]
-      25.times {GildedRose.new(items).update_quality()}
-      expect(items[0].quality).to eq(0)
-    end
+      end
+
   end
 end
